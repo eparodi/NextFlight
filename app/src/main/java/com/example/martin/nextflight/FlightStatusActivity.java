ed1391ec4bb1723c3cea6f8fdde72c10806a0062
Martin <mbiagini@itba.edu.ar>
2016-11-25 21:22:12 -0300

    More layouts and some behaviours changed

--- app/src/main/java/com/example/martin/nextflight/FlightStatusActivity.java
+++ app/src/main/java/com/example/martin/nextflight/FlightStatusActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,6 +25,8 @@ import com.example.martin.nextflight.elements.Departure;
 import com.example.martin.nextflight.elements.Flight;
 import com.example.martin.nextflight.elements.Status;
 import com.example.martin.nextflight.managers.FileManager;
+import com.example.martin.nextflight.managers.ScreenUtility;
+import com.google.android.gms.vision.text.Text;
 import com.google.gson.Gson;
 
 import org.json.JSONObject;
@@ -42,14 +45,32 @@ import java.util.Date;
 public class FlightStatusActivity extends AppCompatActivity{
 
     Status currentStatus;
-    TextView textStatus;
+
     ImageView imageStatus;
-    TextView textAirline;
     ImageView imageAirline;
+
+    TextView textStatus;
+    TextView textFlightNumber;
+    TextView textAirline;
+
     TextView textFrom;
     TextView textTo;
-    TextView textDeparture;
-    TextView textArrival;
+
+    TextView textDepartureDate;
+    TextView textDepartureHour;
+    TextView textDepartureTerminal;
+    TextView textDepartureGate;
+
+    TextView textArrivalDate;
+    TextView textArrivalHour;
+    TextView textArrivalTerminal;
+    TextView textArrivalGate;
+
+    String flightNumber;
+
+    Menu status_menu;
+
+    ScreenUtility screen_utility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,75 +85,34 @@ public class FlightStatusActivity extends AppCompatActivity{
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
 
-        textStatus = (TextView) findViewById(R.id.status);
+        screen_utility = new ScreenUtility(this);
+
         imageStatus = (ImageView) findViewById(R.id.image_status);
-        textAirline = (TextView) findViewById(R.id.airlineName);
         imageAirline = (ImageView) findViewById(R.id.airline_image);
-        textFrom = (TextView) findViewById(R.id.from_info);
-        textTo = (TextView) findViewById(R.id.to_info);
-        textArrival = (TextView) findViewById(R.id.arrival_info);
-        textDeparture = (TextView) findViewById(R.id.departure_info);
+
+        textStatus = (TextView) findViewById(R.id.status_state_text_view);
+        textFlightNumber = (TextView) findViewById(R.id.status_flight_number_text_view);
+        textAirline = (TextView) findViewById(R.id.status_airline_name_text_view);
+
+        textFrom = (TextView) findViewById(R.id.status_from_text_view);
+        textTo = (TextView) findViewById(R.id.status_to_text_view);
+
+        textDepartureDate = (TextView) findViewById(R.id.status_departure_date_text_view);
+        textDepartureHour = (TextView) findViewById(R.id.status_departure_hour_text_view);
+        textDepartureTerminal = (TextView) findViewById(R.id.status_departure_terminal_text_view);
+        textDepartureGate = (TextView) findViewById(R.id.status_departure_gate_text_view);
+
+        textArrivalDate = (TextView) findViewById(R.id.status_arrival_date_text_view);
+        textArrivalHour = (TextView) findViewById(R.id.status_arrival_hour_text_view);
+        textArrivalTerminal = (TextView) findViewById(R.id.status_arrival_terminal_text_view);
+        textArrivalGate = (TextView) findViewById(R.id.status_arrival_gate_text_view);
 
         Bundle bundle = getIntent().getExtras();
 
         final String airline_id = bundle.getString("AirlineId");
-        final String flight_number = bundle.getString("FlightNumber");
+        flightNumber = bundle.getString("FlightNumber");
 
-        new HttpGetStatus(airline_id,flight_number).execute();
-
-        FloatingActionButton map_button = (FloatingActionButton) findViewById(R.id.map_button);
-        map_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), StatusMapsActivity.class);
-
-                PendingIntent pendingIntent =
-                        TaskStackBuilder.create(getApplicationContext())
-                                .addNextIntentWithParentStack(intent)
-                                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
-                builder.setContentIntent(pendingIntent);
-
-                intent.putExtra("DEPLAT", currentStatus.getDeparture().getAirport().getLatitude());
-                intent.putExtra("DEPLONG", currentStatus.getDeparture().getAirport().getLongitude());
-                intent.putExtra("DEPMARK",currentStatus.getDeparture().getAirport().getDescription());
-                intent.putExtra("ARVLAT", currentStatus.getArrival().getAirport().getLatitude());
-                intent.putExtra("ARVLONG", currentStatus.getArrival().getAirport().getLongitude());
-                intent.putExtra("ARVMARK",currentStatus.getArrival().getAirport().getDescription());
-
-                /*
-                    Bundle bundle = new Bundle();
-                    bundle.putString("FlightNumber", flight_number);
-                    bundle.putString("AirlineId", airline_id);
-
-                    intent.putExtras(bundle); */
-
-                startActivity(intent);
-            }
-        });
-
-        FloatingActionButton review_button = (FloatingActionButton) findViewById(R.id.review_button);
-        review_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ReviewActivity.class);
-
-                PendingIntent pendingIntent =
-                        TaskStackBuilder.create(getApplicationContext())
-                                .addNextIntentWithParentStack(intent)
-                                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
-                builder.setContentIntent(pendingIntent);
-
-                intent.putExtra("FlightNumber", flight_number);
-                intent.putExtra("AirlineId", airline_id);
-                intent.putExtra("AirlineName", currentStatus.getAirline().getAirlineName());
-
-                startActivity(intent);
-            }
-        });
-
-
+        new HttpGetStatus(airline_id,flightNumber).execute();
     }
 
     private void fillTextView(){
@@ -156,6 +136,7 @@ public class FlightStatusActivity extends AppCompatActivity{
 
         Airline airline = currentStatus.getAirline();
         textAirline.setText(airline.getAirlineName());
+        textFlightNumber.setText(flightNumber);
 
         new DownloadImageTask(imageAirline).execute(airline.getLogo());
 
@@ -164,9 +145,6 @@ public class FlightStatusActivity extends AppCompatActivity{
 
         textFrom.setText(departure.getAirport().getDescription());
         textTo.setText(arrival.getAirport().getDescription());
-
-        String departure_data = "";
-        String arrival_data = "";
 
         // In Android page, the API level is 1...
         SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -177,50 +155,61 @@ public class FlightStatusActivity extends AppCompatActivity{
 
         try {
             departure_date = parser.parse(departure.getScheduled_time());
+            textDepartureDate.setText(dateFormat.format(departure_date));
+            textDepartureHour.setText(timeFormat.format(departure_date));
+
             arrival_date = parser.parse(arrival.getScheduled_time());
-            departure_data = dateFormat.format(departure_date) + " | " +
-                    timeFormat.format(departure_date) + "\n";
-            arrival_data = dateFormat.format(arrival_date) + " | " +
-                    timeFormat.format(arrival_date) + "\n";
+            textArrivalDate.setText(dateFormat.format(arrival_date));
+            textArrivalHour.setText(timeFormat.format(arrival_date));
         }catch (Exception e){
             // TODO: Message.
         }
 
-        departure_data += "Terminal: ";
-
         String terminal = departure.getAirport().getTerminal();
         if (terminal == null){
-            departure_data +=  "No hay información disponible\n";
+            if (screen_utility.getWidth() > 700.0) {
+                textDepartureTerminal.setText(R.string.null_information);
+            }else {
+                textDepartureTerminal.setText("--");
+            }
+            textDepartureTerminal.setTextColor(getResources().getColor(R.color.md_purple_400));
         }else{
-            departure_data += terminal + "\n";
+            textDepartureTerminal.setText(terminal);
+        }
+        String gate = departure.getAirport().getGate();
+        if (gate == null) {
+            if (screen_utility.getWidth() > 700.0) {
+                textDepartureGate.setText(R.string.null_information);
+            }else {
+                textDepartureGate.setText("--");
+            }
+            textDepartureGate.setTextColor(getResources().getColor(R.color.md_purple_400));
+        } else{
+            textDepartureGate.setText(gate);
         }
 
-        arrival_data += "Terminal: ";
         terminal = arrival.getAirport().getTerminal();
         if (terminal == null){
-            arrival_data +=  "No hay información disponible\n";
+            if (screen_utility.getWidth() > 700.0) {
+                textArrivalTerminal.setText(R.string.null_information);
+            }else {
+                textArrivalTerminal.setText("--");
+            }
+            textArrivalTerminal.setTextColor(getResources().getColor(R.color.md_purple_400));
         }else{
-            arrival_data += terminal + "\n";
+            textArrivalTerminal.setText(terminal);
         }
-
-        arrival_data += "Puerta: ";
-        String puerta = departure.getAirport().getGate();
-        if (puerta == null){
-            arrival_data +=  "No hay información disponible\n";
-        }else{
-            arrival_data += puerta + "\n";
+        gate = arrival.getAirport().getGate();
+        if (gate == null) {
+            if (screen_utility.getWidth() > 700.0) {
+                textArrivalGate.setText(R.string.null_information);
+            }else {
+                textArrivalGate.setText("--");
+            }
+            textArrivalGate.setTextColor(getResources().getColor(R.color.md_purple_400));
+        } else{
+            textArrivalGate.setText(gate);
         }
-
-        departure_data += "Puerta: ";
-        puerta = arrival.getAirport().getGate();
-        if (puerta == null){
-            departure_data +=  "No hay información disponible\n";
-        }else{
-            departure_data += puerta + "\n";
-        }
-
-        textDeparture.setText(departure_data);
-        textArrival.setText(arrival_data);
 
     }
 
@@ -232,40 +221,80 @@ public class FlightStatusActivity extends AppCompatActivity{
         ((TextView) findViewById(R.id.departure_title)).setText("");
     }
 
-    private void configurateFavButton(){
-        FloatingActionButton fav_button = (FloatingActionButton) findViewById(R.id.favourite_button);
-        Flight f = new Flight(currentStatus.getNumber(),currentStatus.getAirline(),
-                currentStatus.getStatus(),currentStatus.getArrival(),currentStatus.getDeparture());
-        FileManager.startFileManager(getApplicationContext());
-        if (FileManager.checkFlight(f)){
-            fav_button.setImageResource(R.drawable.ic_favorite_black);
-        }else{
-            fav_button.setImageResource(R.drawable.ic_favorite_border_black_24dp);
-        }
-        fav_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                FloatingActionButton fav_button = (FloatingActionButton) findViewById(R.id.favourite_button);
-                Flight f = new Flight(currentStatus.getNumber(),currentStatus.getAirline(),
-                        currentStatus.getStatus(),currentStatus.getArrival(),currentStatus.getDeparture());
-                if (FileManager.checkFlight(f)){
-                    fav_button.setImageResource(R.drawable.ic_favorite_border_black_24dp);
-                    FileManager.removeFlight(f,getApplicationContext());
-                }else{
-                    fav_button.setImageResource(R.drawable.ic_favorite_black);
-                    FileManager.addFlight(f,getApplicationContext());
-                }
-            }
-        });
-    }
-
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 return true;
+            case R.id.action_reviews_action_icon:
+                Intent review_intent = new Intent(getApplicationContext(), ReviewActivity.class);
+
+                PendingIntent review_pendingIntent =
+                        TaskStackBuilder.create(getApplicationContext())
+                                .addNextIntentWithParentStack(review_intent)
+                                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                NotificationCompat.Builder review_builder = new NotificationCompat.Builder(getApplicationContext());
+                review_builder.setContentIntent(review_pendingIntent);
+
+                review_intent.putExtra("FlightNumber", currentStatus.getNumber());
+                review_intent.putExtra("AirlineId", currentStatus.getAirline().getAirlineId());
+                review_intent.putExtra("AirlineName", currentStatus.getAirline().getAirlineName());
+
+                startActivity(review_intent);
+                return true;
+            case R.id.action_favorite_action_icon:
+                Flight f = new Flight(currentStatus.getNumber(),currentStatus.getAirline(),
+                        currentStatus.getStatus(),currentStatus.getArrival(),currentStatus.getDeparture());
+                if (FileManager.checkFlight(f)){
+                    item.setIcon(R.drawable.ic_favorite_border_black_24dp);
+                    item.setTitle(getResources().getString(R.string.action_favorite_forget));
+                    FileManager.removeFlight(f,getApplicationContext());
+                }else{
+                    item.setIcon(R.drawable.ic_favorite_black);
+                    item.setTitle(getResources().getString(R.string.action_favorite));
+                    FileManager.addFlight(f,getApplicationContext());
+                }
+                return true;
+            case R.id.action_status_map_action_icon:
+                Intent map_intent = new Intent(getApplicationContext(), StatusMapsActivity.class);
+
+                PendingIntent map_pendingIntent =
+                        TaskStackBuilder.create(getApplicationContext())
+                                .addNextIntentWithParentStack(map_intent)
+                                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                NotificationCompat.Builder map_builder = new NotificationCompat.Builder(getApplicationContext());
+                map_builder.setContentIntent(map_pendingIntent);
+
+                map_intent.putExtra("DEPLAT", currentStatus.getDeparture().getAirport().getLatitude());
+                map_intent.putExtra("DEPLONG", currentStatus.getDeparture().getAirport().getLongitude());
+                map_intent.putExtra("DEPMARK",currentStatus.getDeparture().getAirport().getDescription());
+                map_intent.putExtra("ARVLAT", currentStatus.getArrival().getAirport().getLatitude());
+                map_intent.putExtra("ARVLONG", currentStatus.getArrival().getAirport().getLongitude());
+                map_intent.putExtra("ARVMARK",currentStatus.getArrival().getAirport().getDescription());
+
+                /*
+                    Bundle bundle = new Bundle();
+                    bundle.putString("FlightNumber", flight_number);
+                    bundle.putString("AirlineId", airline_id);
+
+                    intent.putExtras(bundle); */
+
+                startActivity(map_intent);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.flight_status, menu);
+        status_menu = menu;
+        return true;
+    }
+
     private class HttpGetStatus extends AsyncTask<Void, Void, String> {
 
         private static final String STATUS = "status";
@@ -316,12 +345,22 @@ public class FlightStatusActivity extends AppCompatActivity{
                 currentStatus = gson.fromJson(jsonFragment, com.example.martin.nextflight.elements.Status.class);
 
                 fillTextView();
-                configurateFavButton();
+                Flight f = new Flight(currentStatus.getNumber(),currentStatus.getAirline(),
+                        currentStatus.getStatus(),currentStatus.getArrival(),currentStatus.getDeparture());
+                FileManager.startFileManager(getApplicationContext());
+                MenuItem favorite_item = status_menu.findItem(R.id.action_favorite_action_icon);
+                if (FileManager.checkFlight(f)){
+                    favorite_item.setIcon(R.drawable.ic_favorite_black);
+                    favorite_item.setTitle(getResources().getString(R.string.action_favorite));
+                }else{
+                    favorite_item.setIcon(R.drawable.ic_favorite_border_black_24dp);
+                    favorite_item.setTitle(getResources().getString(R.string.action_favorite_forget));
+                }
             } catch (Exception e) {
                 Toast.makeText(getApplicationContext(),
                         R.string.flight_unknown,
                         Toast.LENGTH_SHORT).show();
-                hideAll();
+                //hideAll();
                 finish();
             } finally {
 
